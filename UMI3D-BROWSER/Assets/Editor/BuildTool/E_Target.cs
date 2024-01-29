aeea3e15036dfc2e505f2701f653b4401233d738
Romain Tirbisch <romain.tirbisch@inetum.com>
2024-01-29 11:57:30 +0100

    Update build tool.

--- UMI3D-BROWSER/Assets/Editor/BuildTool/E_Target.cs
+++ UMI3D-BROWSER/Assets/Editor/BuildTool/E_Target.cs
@@ -14,14 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace BuildTool
+namespace umi3d.browserEditor.BuildTool
 {
     public enum E_Target
     {
-        Quest, Focus, Pico, SteamXR
+        Quest, 
+        Focus, 
+        Pico, 
+        SteamXR
     }
 }

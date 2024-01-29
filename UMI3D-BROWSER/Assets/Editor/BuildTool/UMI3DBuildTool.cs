40c4e1fad8ef3d528dc11647cfd5bc45b3095a04
Romain Tirbisch <romain.tirbisch@inetum.com>
2024-01-29 17:55:32 +0100

    Fix switch target issus when building.

--- UMI3D-BROWSER/Assets/Editor/BuildTool/UMI3DBuildTool.cs
+++ UMI3D-BROWSER/Assets/Editor/BuildTool/UMI3DBuildTool.cs
@@ -49,17 +49,45 @@ namespace umi3d.browserEditor.BuildTool
             VisualElement ui_instance = ui.Instantiate();
 
             root.Add(ui_instance);
+            TextField TF_BuildName = root.Q<TextField>("TF_BuildName");
+            DropdownField DD_TargetSelection = root.Q<DropdownField>("DD_TargetSelection");
+            DropdownField DD_ReleaseCycle = root.Q<DropdownField>("DD_ReleaseCycle");
+            VisualElement V_Major = root.Q("V_Major");
+            IntegerField IF_Major = V_Major.Q<IntegerField>();
+            Button B_Major = V_Major.Q<Button>();
+            VisualElement V_Minor = root.Q("V_Minor");
+            IntegerField IF_Minor = V_Minor.Q<IntegerField>();
+            Button B_Minor = V_Minor.Q<Button>();
+            VisualElement V_BuildCount = root.Q("V_BuildCount");
+            IntegerField IF_BuildCount = V_BuildCount.Q<IntegerField>();
+            Button B_BuildCount = V_BuildCount.Q<Button>();
+            TextField TF_AdditionalVersion = root.Q<TextField>("TF_AdditionalVersion");
+            Button B_ResetVersion = root.Q<Button>("Reset");
+            Label L_OldVersion = root.Q<Label>("L_OldVersion");
+            Label L_Version = root.Q<Label>("L_BuildVersion");
+            Button B_Apply = root.Q<Button>("B_Apply");
+            Button B_Build = root.Q<Button>("B_Build");
+
+            void ApplyChange()
+            {
+                B_Apply.SetEnabled(false);
+                B_Build.SetEnabled(true);
+            }
+            void WaitChangeValidation()
+            {
+                B_Apply.SetEnabled(true);
+                B_Build.SetEnabled(false);
+            }
 
             // Build name.
-            TextField TF_BuildName = root.Q<TextField>("TF_BuildName");
             TF_BuildName.value = PlayerSettings.productName;
             TF_BuildName.RegisterValueChangedCallback(value => 
             { 
                 PlayerSettings.productName = value.newValue;
+                WaitChangeValidation();
             });
 
             // Device target.
-            DropdownField DD_TargetSelection = root.Q<DropdownField>("DD_TargetSelection");
             DD_TargetSelection.choices.Clear();
             DD_TargetSelection.choices.AddRange(Enum.GetNames(typeof(E_Target)));
             DD_TargetSelection.value = DD_TargetSelection.choices[(int)buildTool_SO.Target];
@@ -67,10 +95,10 @@ namespace umi3d.browserEditor.BuildTool
             {
                 buildTool_SO.Target = Enum.Parse<E_Target>(value.newValue);
                 selectedTarget = buildTool_SO.Target;
+                WaitChangeValidation();
             });
 
             // Release cycle.
-            DropdownField DD_ReleaseCycle = root.Q<DropdownField>("DD_ReleaseCycle");
             DD_ReleaseCycle.choices.Clear();
             DD_ReleaseCycle.choices.AddRange(Enum.GetNames(typeof(E_ReleaseCycle)));
             DD_ReleaseCycle.value = DD_ReleaseCycle.choices[(int)buildTool_SO.releaseCycle];
@@ -78,12 +106,10 @@ namespace umi3d.browserEditor.BuildTool
             {
                 buildTool_SO.releaseCycle = Enum.Parse<E_ReleaseCycle>(value.newValue);
                 buildTool_SO.updateVersion?.Invoke();
+                WaitChangeValidation();
             });
 
             // Major version.
-            VisualElement V_Major = root.Q("V_Major");
-            IntegerField IF_Major = V_Major.Q<IntegerField>();
-            Button B_Major = V_Major.Q<Button>();
             IF_Major.value = buildTool_SO.majorVersion;
             B_Major.clicked += () =>
             {
@@ -93,12 +119,10 @@ namespace umi3d.browserEditor.BuildTool
             {
                 buildTool_SO.majorVersion = IF_Major.value;
                 buildTool_SO.updateVersion?.Invoke();
+                WaitChangeValidation();
             });
 
             // Minor version
-            VisualElement V_Minor = root.Q("V_Minor");
-            IntegerField IF_Minor = V_Minor.Q<IntegerField>();
-            Button B_Minor = V_Minor.Q<Button>();
             IF_Minor.value = buildTool_SO.minorVersion;
             B_Minor.clicked += () =>
             {
@@ -108,12 +132,10 @@ namespace umi3d.browserEditor.BuildTool
             {
                 buildTool_SO.minorVersion = IF_Minor.value;
                 buildTool_SO.updateVersion?.Invoke();
+                WaitChangeValidation();
             });
 
             // Build count.
-            VisualElement V_BuildCount = root.Q("V_BuildCount");
-            IntegerField IF_BuildCount = V_BuildCount.Q<IntegerField>();
-            Button B_BuildCount = V_BuildCount.Q<Button>();
             IF_BuildCount.value = buildTool_SO.buildCountVersion;
             B_BuildCount.clicked += () =>
             {
@@ -123,20 +145,20 @@ namespace umi3d.browserEditor.BuildTool
             {
                 buildTool_SO.buildCountVersion = IF_BuildCount.value;
                 buildTool_SO.updateVersion?.Invoke();
+                WaitChangeValidation();
             });
 
             // Additional information.
             // Like if you want to add unity editor version.
-            TextField TF_AdditionalVersion = root.Q<TextField>("TF_AdditionalVersion");
             TF_AdditionalVersion.value = buildTool_SO.additionalVersion;
             TF_AdditionalVersion.RegisterValueChangedCallback((value) =>
             {
                 buildTool_SO.additionalVersion = value.newValue;
                 buildTool_SO.updateVersion?.Invoke();
+                WaitChangeValidation();
             });
 
             // Reset version.
-            Button B_ResetVersion = root.Q<Button>("Reset");
             B_ResetVersion.clicked += () =>
             {
                 IF_Major.value = buildTool_SO.oldMajorVersion;
@@ -144,14 +166,12 @@ namespace umi3d.browserEditor.BuildTool
                 IF_BuildCount.value = buildTool_SO.oldBuildCountVersion;
             };
 
-            Label L_OldVersion = root.Q<Label>("L_OldVersion");
             L_OldVersion.text = buildTool_SO.OldVersion;
             buildTool_SO.updateOldVersion = () =>
             {
                 L_OldVersion.text = buildTool_SO.OldVersion;
             };
 
-            Label L_Version = root.Q<Label>("L_BuildVersion");
             buildTool_SO.updateVersion = () =>
             {
                 L_Version.text = buildTool_SO.Version;
@@ -159,7 +179,12 @@ namespace umi3d.browserEditor.BuildTool
                 PlayerSettings.Android.bundleVersionCode = buildTool_SO.BundleVersion;
             };
 
-            Button B_Build = root.Q<Button>("B_Build");
+            B_Build.SetEnabled(false);
+            B_Apply.clicked += () =>
+            {
+                ApplyChange();
+                this.ApplyChange();
+            };
             B_Build.clicked += () =>
             {
                 buildTool_SO.UpdateOldVersionWithNewVersion();
@@ -169,10 +194,14 @@ namespace umi3d.browserEditor.BuildTool
             buildTool_SO.updateVersion?.Invoke();
         } 
         
-        private void Build()
+        private void ApplyChange()
         {
             _uMI3DConfigurator.HandleTarget(selectedTarget);
             _targetAndPluginSwitcher.HandleTarget(selectedTarget);
+        }
+
+        private void Build()
+        {
             BuildPipeline.BuildPlayer(BuildToolHelper.GetPlayerBuildOptions());
         }
     }

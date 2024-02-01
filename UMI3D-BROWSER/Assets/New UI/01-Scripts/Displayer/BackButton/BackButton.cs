98f34d6130812923ba9c02e775afbe0c02cb5bd6
Arthur-Aberth <arthur.aberth@inetum.com>
2024-02-01 14:40:29 +0100

    Create back button

---
+++ UMI3D-BROWSER/Assets/New UI/01-Scripts/Displayer/BackButton/BackButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BackButton : Selectable, IPointerDownHandler, IPointerUpHandler
+{
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // A relier avec la logique de déplacement dans l'UI
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // A relier avec la logique de déplacement dans l'UI
+    }
+}

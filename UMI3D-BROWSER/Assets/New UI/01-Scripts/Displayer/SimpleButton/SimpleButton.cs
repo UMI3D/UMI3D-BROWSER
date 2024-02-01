cc07d8f2a360b879feed9049476bb93e6169a7bb
Arthur-Aberth <arthur.aberth@inetum.com>
2024-02-01 15:22:19 +0100

    Add simple button

---
+++ UMI3D-BROWSER/Assets/New UI/01-Scripts/Displayer/SimpleButton/SimpleButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using inetum.unityUtils;
+
+public class SimpleButton : Selectable
+{
+    public UnityEvent OnClick;
+    public UnityEvent OnRelease;
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        OnClick.Invoke();
+    }
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        OnRelease.Invoke();
+    }
+
+}

14efcd99515d01e59894babc031a7efc3ba6fbe7
Charles Marichal <charles-eole.marichal@inetum.com>
2024-03-06 11:11:20 +0100

    Add dummy grab cursor to silent nullref

---
+++ UMI3D-BROWSER/Assets/01-VR Browser/Project/Scripts/GestureDetection/DummyGrabCursor.cs
@@ -0,0 +1,22 @@
+using umi3dBrowsers.interaction.selection;
+using umi3dBrowsers.interaction.selection.cursor;
+
+using UnityEngine;
+
+public class DummyGrabCursor : AbstractCursor
+{ 
+    public override void ChangeAccordingToSelection(AbstractSelectionData selectedObjectInfo)
+    {
+        // do nothing
+    }
+
+    public override void Display()
+    {
+        // do nothing
+    }
+
+    public override void Hide()
+    {
+        // do nothing
+    }
+}

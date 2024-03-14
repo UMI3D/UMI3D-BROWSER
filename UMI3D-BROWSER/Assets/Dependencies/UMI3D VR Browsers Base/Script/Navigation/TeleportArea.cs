72328d6628fea621ee478ea5771d0a3bfb52c583
Romain T. <romain.tirbisch@inetum.com>
2024-03-14 09:57:00 +0100

    Fix one issue with the teleportation provider.

--- UMI3D-BROWSER/Assets/Dependencies/UMI3D VR Browsers Base/Script/Navigation/TeleportArea.cs
+++ UMI3D-BROWSER/Assets/Dependencies/UMI3D VR Browsers Base/Script/Navigation/TeleportArea.cs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using umi3d.browserRuntime.interaction;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 

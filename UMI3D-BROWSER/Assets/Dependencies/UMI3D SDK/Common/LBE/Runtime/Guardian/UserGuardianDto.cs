a7cdea11d1182eb910eeb9c3f94e1d369856876c
Rémi Bréhier <remi.brehier@inetum.com>
2024-03-22 14:11:47 +0100

    Add data guardian in SDK

---
+++ UMI3D-BROWSER/Assets/Dependencies/UMI3D SDK/Common/LBE/Runtime/Guardian/UserGuardianDto.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using umi3d.common.userCapture.description;
+using System;
+
+
+namespace umi3d.common.lbe.description
+
+{
+    public class UserGuardianDto : MonoBehaviour
+    {
+        public List<ARAnchorDto> anchorAR { get; set; }
+    }
+}
+

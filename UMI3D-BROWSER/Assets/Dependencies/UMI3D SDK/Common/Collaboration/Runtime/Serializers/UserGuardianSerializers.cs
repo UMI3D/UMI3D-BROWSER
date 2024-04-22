7f518e1c5ec42cd80ab59cc9685d3e8281fe92b1
Rémi Bréhier <remi.brehier@inetum.com>
2024-04-22 17:16:56 +0200

    Calibration environnement

--- UMI3D-BROWSER/Assets/Dependencies/UMI3D SDK/Common/Collaboration/Runtime/Serializers/UserGuardianSerializers.cs
+++ UMI3D-BROWSER/Assets/Dependencies/UMI3D SDK/Common/Collaboration/Runtime/Serializers/UserGuardianSerializers.cs
@@ -12,14 +12,11 @@ namespace umi3d.common.lbe.guardian
 
         public bool? IsCountable<T>()
         {
-            Debug.Log("Remi : Countable");
             return typeof(T) == typeof(UserGuardianDto) ? true : null;
         }
 
         public bool Read<T>(ByteContainer container, out bool readable, out T result)
         {
-            Debug.Log("Remi : Read 1");
-
             if (typeof(T) == typeof(UserGuardianDto))
             {
                 List<ARAnchorDto> anchorARList = UMI3DSerializer.ReadList<ARAnchorDto>(container);
@@ -44,13 +41,8 @@ namespace umi3d.common.lbe.guardian
 
         public bool Write<T>(T value, out Bytable bytable, params object[] parameters)
         {
-            Debug.Log("Remi : Write 1");
-
             if (value is UserGuardianDto c)
             {
-                Debug.Log("Remi : Write 2");
-
-                // Ecriture de la liste anchorAR
                 bytable = UMI3DSerializer.Write(UMI3DOperationKeys.GuardianBrowserRequest)
                     + UMI3DSerializer.WriteCollection(c.anchorAR);
                 return true;

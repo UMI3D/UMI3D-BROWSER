81fcdffc5ec32e1199684da0e1ec98aed5284205
vladimir.chantitch@inetum.com <VladGitHub22*>
2024-04-26 13:38:04 +0200

    Enabeling skybox for loading

---
+++ UMI3D-BROWSER/Assets/New UI/01-Scripts/Services/Environment/LoadingEnvironmentHandler.cs
@@ -0,0 +1,75 @@
+﻿/*
+Copyright 2019 - 2022 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using inetum.unityUtils;
+using umi3d.cdk;
+using umi3dVRBrowsersBase.ui.playerMenu;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace umi3dBrowsers.services.environment
+{
+    public class LoadingEnvironmentHandler : PersistentSingleBehaviour<LoadingEnvironmentHandler>
+    {
+        private Camera cam;
+
+        [SerializeField] private LayerMask loadingCullingMask;      
+        private int defaultCullingMask;
+
+        [SerializeField] private Material skyBoxHomeMaterial;
+
+        /// <summary>
+        /// Event raised when loading screen is displayed.
+        /// </summary>
+        public static UnityEvent OnLoadingScreenDislayed = new UnityEvent();
+
+        /// <summary>
+        /// Event raised when loading screen is hidden.
+        /// </summary>
+        public static UnityEvent OnLoadingScreenHidden = new UnityEvent();
+
+        private void Start()
+        {
+            cam = Camera.main;
+            Debug.Assert(cam != null, "Impossible to find a camera");
+
+            defaultCullingMask = cam.cullingMask;
+
+            Stop();
+        }
+
+        /// <summary>
+        /// Displays loading screen
+        /// </summary>
+        public void IsLoading()
+        {
+            cam.cullingMask = loadingCullingMask.value;
+            cam.clearFlags = CameraClearFlags.Skybox;
+            RenderSettings.skybox = skyBoxHomeMaterial;
+        }
+
+        /// <summary>
+        /// Hides loading screen
+        /// </summary>
+        public void Stop()
+        {
+            cam.cullingMask = defaultCullingMask;
+            cam.clearFlags = CameraClearFlags.Skybox;
+
+            OnLoadingScreenHidden?.Invoke();
+        }
+    }
+}

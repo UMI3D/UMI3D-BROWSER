aa0eed24272559a892f33dbc54c1580639f5c3b6
vladimir.chantitch@inetum.com <VladGitHub22*>
2024-09-03 15:37:14 +0200

    SpawnPointLayout ok

---
+++ UMI3D-BROWSER/Assets/PC/Project/Scripts/PCInGameUI/Map/SpawnPointUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace umi3dBrowsers.ingame_ui
+{
+    public class SpawnPointUI : MonoBehaviour
+    {
+        public void Init(string locationName, int id)
+        {
+
+        }
+    }
+}
+

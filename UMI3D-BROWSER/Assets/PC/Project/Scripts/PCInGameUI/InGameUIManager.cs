c79e881147188db20210b6bf9294001761f925f4
Guyon Nolan <133751460+nguyon-inetum@users.noreply.github.com>
2024-09-05 16:16:55 +0200

    fix: Mouse left click

--- UMI3D-BROWSER/Assets/PC/Project/Scripts/PCInGameUI/InGameUIManager.cs
+++ UMI3D-BROWSER/Assets/PC/Project/Scripts/PCInGameUI/InGameUIManager.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using umi3d.baseBrowser.cursor;
+using umi3d.baseBrowser.inputs.interactions;
 using umi3dBrowsers.linker.ingameui;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static umi3d.baseBrowser.cursor.BaseCursor;
 
 namespace umi3dBrowsers.ingame_ui
 {
@@ -30,6 +33,21 @@ namespace umi3dBrowsers.ingame_ui
             inGameLinker.OnEnableDisableInGameUI += isEnable => gameObject.SetActive(isEnable);
             if (!debugMode)
                 gameObject.SetActive(inGameLinker.IsEnable);
+
+
+
+            KeyboardShortcut.AddUpListener(ShortcutEnum.FreeCursor, () => {
+                if (mainInGamePanel.gameObject.activeSelf)
+                    return;
+
+                if (BaseCursor.Movement == CursorMovement.Center)
+                    BaseCursor.SetMovement(this, CursorMovement.Free);
+                else
+                {
+                    BaseCursor.SetMovement(this, CursorMovement.Center);
+                    BaseCursor.State = CursorState.Default;
+                }
+            });
         }
 
         private void ToggleInGamePanel()
@@ -39,11 +57,13 @@ namespace umi3dBrowsers.ingame_ui
                 if (mainInGamePanel.gameObject.activeSelf)
                 {
                     Cursor.lockState = CursorLockMode.Locked;
+                    BaseCursor.SetMovement(this, CursorMovement.Center);
                     mainInGamePanel.gameObject.SetActive(false);
                 }
                 else
                 {
                     Cursor.lockState = CursorLockMode.Confined;
+                    BaseCursor.SetMovement(this, CursorMovement.Free);
                     mainInGamePanel.gameObject.SetActive(true);
                 }
             }

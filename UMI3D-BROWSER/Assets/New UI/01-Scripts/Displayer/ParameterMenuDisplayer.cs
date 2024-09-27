65043e092e3741de7b50dab6708cc68e4a48686a
Guyon Nolan <133751460+nguyon-inetum@users.noreply.github.com>
2024-09-27 10:51:08 +0200

    feat(InGameMenu: ParameterMenu): Add Dropdown + Slider parameter controls

--- UMI3D-BROWSER/Assets/New UI/01-Scripts/Displayer/ParameterMenuDisplayer.cs
+++ UMI3D-BROWSER/Assets/New UI/01-Scripts/Displayer/ParameterMenuDisplayer.cs
@@ -15,12 +15,15 @@ limitations under the License.
 */
 
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using umi3d.baseBrowser.Controller;
 using umi3d.baseBrowser.cursor;
+using umi3d.baseBrowser.parameters;
 using umi3d.common.interaction;
 using umi3dBrowsers.displayer;
 using UnityEngine;
+using UnityEngine.UI;
 using static umi3d.baseBrowser.cursor.BaseCursor;
 
 public class ParameterMenuDisplayer : MonoBehaviour
@@ -28,6 +31,8 @@ public class ParameterMenuDisplayer : MonoBehaviour
     [SerializeField] private Transform content;
     [SerializeField] private GameObject stringParameterDisplayPrefab;
     [SerializeField] private GameObject booleanParameterDisplayPrefab;
+    [SerializeField] private GameObject sliderParameterDisplayPrefab;
+    [SerializeField] private GameObject dropdownParameterDisplayPrefab;
 
     private void Awake()
     {
@@ -43,8 +48,10 @@ public class ParameterMenuDisplayer : MonoBehaviour
 
     private void Show(List<AbstractParameterDto> parameters)
     {
-        if (gameObject.activeInHierarchy)
+        if (gameObject.activeInHierarchy || parameters.Count <= 0)
             return;
+
+        parameters.Reverse(); // Reverse to show element above in front (layout in the object is set to reverse too)
 
         gameObject.SetActive(true);
         BaseCursor.SetMovement(this, CursorMovement.Free);
@@ -82,6 +89,46 @@ public class ParameterMenuDisplayer : MonoBehaviour
                 toggle.onToggleOff.AddListener(() => {
                     booleanParameter.value = false;
                 });
+                return;
+            }
+            case FloatRangeParameterDto sliderParameter:
+            {
+                var sliderGameObject = Instantiate(sliderParameterDisplayPrefab, content);
+                sliderGameObject.GetComponentInChildren<TMP_Text>().text = sliderParameter.name;
+                var slider = sliderGameObject.GetComponentInChildren<Slider>();
+                slider.value = sliderParameter.value;
+                slider.minValue = sliderParameter.min;
+                slider.maxValue = sliderParameter.max;
+                slider.wholeNumbers = false;
+                slider.onValueChanged.AddListener(newValue => {
+                    sliderParameter.value = newValue;
+                });
+                return;
+            }
+            case IntegerRangeParameterDto sliderParameter:
+            {
+                var sliderGameObject = Instantiate(sliderParameterDisplayPrefab, content);
+                sliderGameObject.GetComponentInChildren<TMP_Text>().text = sliderParameter.name;
+                var slider = sliderGameObject.GetComponentInChildren<Slider>();
+                slider.value = sliderParameter.value;
+                slider.minValue = sliderParameter.min;
+                slider.maxValue = sliderParameter.max;
+                slider.wholeNumbers = true;
+                slider.onValueChanged.AddListener(newValue => {
+                    sliderParameter.value = (int)newValue;
+                });
+                return;
+            }
+            case EnumParameterDto<string> dropdownParameter:
+            {
+                var dropdownGameObject = Instantiate(dropdownParameterDisplayPrefab, content);
+                dropdownGameObject.GetComponentInChildren<TMP_Text>().text = dropdownParameter.name;
+                var dropdown = dropdownGameObject.GetComponentInChildren<GridDropDown>();
+                dropdown.Init(dropdownParameter.possibleValues.Select(v => new GridDropDownItemCell(v)).ToList(),
+                    dropdownParameter.possibleValues.IndexOf(dropdownParameter.value));
+                dropdown.OnClick += () => {
+                    dropdownParameter.value = dropdown.GetValue();
+                };
                 return;
             }
             default:

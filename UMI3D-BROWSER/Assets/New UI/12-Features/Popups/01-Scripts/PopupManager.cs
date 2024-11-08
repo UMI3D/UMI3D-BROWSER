19127da09643471fe223bdcc96e67cc2bba31efa
Romain T. <romain.tirbisch@inetum.com>
2024-11-08 18:32:57 +0100

    WIP: popup notification in popupManager

--- UMI3D-BROWSER/Assets/New UI/12-Features/Popups/01-Scripts/PopupManager.cs
+++ UMI3D-BROWSER/Assets/New UI/12-Features/Popups/01-Scripts/PopupManager.cs
@@ -14,32 +14,103 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using inetum.unityUtils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace umi3d
+namespace umi3d.browserRuntime.ui.popup
 {
     public class PopupManager : MonoBehaviour
     {
+        [SerializeField] GameObject popupPrefab;
+
+        GameObject popup;
+        List<Dictionary<string, System.Object>> popupInfo = new();
+
         void Awake()
         {
-        }
+            NotificationHub.Default
+                .Subscribe<PopupNotificationKeys.Show>(
+                this,
+                new FilterByRef(FilterType.AcceptAllExcept, this),
+                NewPopupEnqueued
+            );
 
-        void OnEnable()
-        {
-        }
+            NotificationHub.Default
+                .Subscribe<PopupNotificationKeys.PopupClosed>(this, PopupClosed);
 
-        void OnDisable()
-        {
+            popup = Instantiate(popupPrefab);
+            transform.SetParent(popup.transform, false);
+            popup.SetActive(false);
         }
 
         void OnDestroy()
         {
+            NotificationHub.Default
+             .Unsubscribe<PopupNotificationKeys.Show>(this);
         }
 
-        void Update()
+        void NewPopupEnqueued(Notification notification)
         {
+            Dictionary<string, System.Object> info = new(notification.Info);
+            popupInfo.Add(info);
+
+            if (!popup.activeInHierarchy)
+            {
+                DisplayNextPopup();
+            }
         }
+
+        void PopupClosed()
+        {
+            popup.SetActive(false);
+            DisplayNextPopup();
+        }
+
+        void DisplayNextPopup()
+        {
+            if (popupInfo.Count == 0)
+            {
+                return;
+            }
+
+            Dictionary<string, System.Object> info = null;
+            for (int i = 0; i < popupInfo.Count; i++)
+            {
+                Dictionary<string, object> _info = popupInfo[i];
+                if (!_info.TryGetValue(PopupNotificationKeys.Show.Type, out System.Object type))
+                {
+                    continue;
+                }
+
+                if (type is not PopupType popupType)
+                {
+                    continue; 
+                }
+
+                if (popupType == PopupType.Error)
+                {
+                    info = _info;
+                    break;
+                }
+
+                if (popupType == PopupType.Warning && info == null)
+                {
+                    info = _info;
+                }
+            }
+
+            if (info == null)
+            {
+                info = popupInfo[0];
+            }
+            popupInfo.Remove(info);
+
+            NotificationHub.Default.Notify<PopupNotificationKeys.Show>(this, info);
+
+            popup.SetActive(true);
+        }
+
     }
 }
--- UMI3D-BROWSER/Assets/Runtime/NotificationKeys/PopupNotificationKeys.cs
+++ UMI3D-BROWSER/Assets/Runtime/NotificationKeys/PopupNotificationKeys.cs
@@ -27,38 +27,62 @@ namespace umi3d.browserRuntime.ui.popup
             /// Type of the popup.
             /// </summary>
             /// <remarks>
-            /// Value type : <see cref="Type"/>
+            /// Value type : <see cref="PopupType"/>
             /// </remarks>
-            public const string Type = "popup-type";
+            public const string Type = "Type";
+
             /// <summary>
-            /// The localized key for the title of the popup
+            /// The title of the popup.
             /// </summary>
             /// <remarks>
-            /// Value type : <see cref="string"/>
+            /// Value type : string <see cref="string"/> or localized string (<see cref="string"/> Table, <see cref="string"/> Key)
             /// </remarks>
-            public const string Title = "popup-title";
+            public const string Title = "Title";
+
             /// <summary>
-            /// The localized key for the description of the popup
+            /// The description of the popup.
             /// </summary>
             /// <remarks>
-            /// Value type : <see cref="string"/>
+            /// Value type : string <see cref="string"/> or localized string (<see cref="string"/> Table, <see cref="string"/> Key)
             /// </remarks>
-            public const string Description = "popup-description";
+            public const string Description = "Description";
+
             /// <summary>
-            /// A list of buttons to show under the text of the popup
+            /// A list of buttons to show under the text of the popup.
             /// </summary>
             /// <remarks>
-            /// Value type : List(<see cref="string"/> "text", <see cref="System.Action"/> "onClick")
+            /// Value type : List(<see cref="System.Object"/>) where object can be string <see cref="string"/> or localized string (<see cref="string"/> Table, <see cref="string"/> Key)
             /// </remarks>
-            public const string Buttons = "popup-buttons";
+            public const string Buttons = "Buttons";
+
+            /// <summary>
+            /// The action trigger when a button is clicked.
+            /// </summary>
+            /// <remarks>
+            /// Value type : <see cref="System.Action"/>(<see cref="int"/>)<br/>
+            /// -1 means that the close button of the popup has been pressed.<br/>
+            /// 0 means that the first button has been pressed, and so on.
+            /// </remarks>
+            public const string ButtonActions = "ButtonActions";
+
             /// <summary>
             /// Arguments used by localisation in <see cref="Title"/> and <see cref="Description"/>
             /// </summary>
             /// <remarks>
             /// Value type : Dictionary(<see cref="string"/> "key", <see cref="object"/> "value")
             /// </remarks>
-            public const string Arguments = "popup-arguments";
+            public const string Arguments = "Arguments";
         }
+
+        /// <summary>
+        /// Notification when a popup has closed.
+        /// </summary>
+        public class PopupClosed { }
+
+        /// <summary>
+        /// Event raised when all the pop up have been closed.
+        /// </summary>
+        public class AllPopupAreClosed { }
 
         /// <summary>
         /// Close all opened popup;

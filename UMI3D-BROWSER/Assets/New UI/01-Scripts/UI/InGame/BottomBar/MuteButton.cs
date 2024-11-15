2af1a42f15905e21b808b38ef23978f6e00ca580
Guyon Nolan <133751460+nguyon-inetum@users.noreply.github.com>
2024-11-15 16:07:48 +0100

    feat(Shortcuts): Set mute shorcut

--- UMI3D-BROWSER/Assets/New UI/01-Scripts/UI/InGame/BottomBar/MuteButton.cs
+++ UMI3D-BROWSER/Assets/New UI/01-Scripts/UI/InGame/BottomBar/MuteButton.cs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using inetum.unityUtils;
 using umi3d.cdk.collaboration;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -31,6 +32,8 @@ namespace umi3d.browserRuntime.ui.inGame.bottomBar
         private void Awake()
         {
             button.onClick.AddListener(Mute);
+
+            NotificationHub.Default.Subscribe(this, InGameNotificationKeys.MuteChanged, UpdateIcon);
         }
 
         private void OnEnable()

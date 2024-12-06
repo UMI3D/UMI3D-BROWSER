854b4545bc245b66458c914cd9447bec847276a4
Guyon Nolan <133751460+nguyon-inetum@users.noreply.github.com>
2024-12-06 10:17:43 +0100

    feat(Shortcut): Hide / Show Ui

---
+++ UMI3D-BROWSER/Assets/New UI/12-Features/Shortcuts/01-Scripts/UiVisibilityShortcut.cs
@@ -0,0 +1,41 @@
+/*
+Copyright 2019 - 2024 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using inetum.unityUtils;
+using umi3d.baseBrowser.inputs.interactions;
+using umi3d.browserRuntime.ui;
+using UnityEngine;
+
+namespace umi3d.browserRuntime.shortcuts
+{
+    public class UiVisibilityShortcut : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            KeyboardShortcut.AddDownListener(ShortcutEnum.ToggleUiVisibility, ToggleUiVisibility);
+        }
+
+        private void OnDisable()
+        {
+            KeyboardShortcut.RemoveDownListener(ShortcutEnum.ToggleUiVisibility, ToggleUiVisibility);
+        }
+
+        private void ToggleUiVisibility()
+        {
+            NotificationHub.Default.Notify(this, UiNotificationKeys.ToggleVisibility);
+        }
+    }
+}

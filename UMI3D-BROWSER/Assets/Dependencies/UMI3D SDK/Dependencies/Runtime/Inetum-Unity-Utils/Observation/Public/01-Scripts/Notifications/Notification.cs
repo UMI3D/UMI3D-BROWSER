5bbdb46dfd235467dcf073e610be6faf8e1e8229
Romain T. <romain.tirbisch@inetum.com>
2024-12-23 15:58:27 +0100

    inetum.unityUtils.observation
    - Partial test fot Notification class.
    -> Test for constructor and TryGetInfo and TryGetInfoT

--- UMI3D-BROWSER/Assets/Dependencies/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Observation/Public/01-Scripts/Notifications/Notification.cs
+++ UMI3D-BROWSER/Assets/Dependencies/UMI3D SDK/Dependencies/Runtime/Inetum-Unity-Utils/Observation/Public/01-Scripts/Notifications/Notification.cs
@@ -44,50 +44,126 @@ namespace inetum.unityUtils.observation
         Notification() { }
 
         /// <summary>
-        /// Notification constructor. Initialize all the information
+        /// Initializes a new instance of the Notification class.<br/>
+        /// Logs an error if the id is null or empty, or if the publisher is null.<br/>
+        /// <br/>
+        /// <example>
+        /// Given an id, publisher, and info when constructing a Notification then create a notification.<br/>
+        /// <code>
+        /// Notification notification = new(id, publisher, info);
+        /// // notification.ID == id
+        /// // notification.Publisher == publisher
+        /// // notification.Info == info
+        /// </code>
+        /// </example>
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="publisher"></param>
-        /// <param name="info"></param>
+        /// <param name="id">The unique identifier for the notification.</param>
+        /// <param name="publisher">The publisher of the notification.</param>
+        /// <param name="info">Additional information related to the notification.</param>
         public Notification(string id, object publisher, Dictionary<string, object> info)
         {
+            if (string.IsNullOrEmpty(id))
+            {
+                UnityEngine.Debug.LogError($"[Notification.Notification] Error: create a new notification with id null or empty.");
+            }
+
+            if (publisher == null)
+            {
+                string message = $"[Notification.Notification] Error: create a new notification with a null publisher.\n" +
+                    $"Having a null publisher is a bad practice because it increase complexity while debugging.";
+                UnityEngine.Debug.LogError(message);
+            }
+
             ID = id;
             Publisher = publisher;
             Info = info;
         }
 
         /// <summary>
-        /// Try to get the information stored with this <paramref name="key"/>.<br/>
-        /// Return true if the information exist, else false.
+        /// Tries to retrieve information associated with a given key from the notification.<br/>
+        /// If the key is null or not found, it logs an error (if logError is true) and returns false.<br/>
+        /// <br/>
+        /// <example>
+        /// Given a notification and a key, when the key is found in the notification's info, then return true and the associated info.<br/>
+        /// <code>
+        /// Notification notification = new Notification("id", this, new() { { "key", "value" } });
+        /// bool result = notification.TryGetInfo("key", out object info); // result = true, info = "value"
+        /// </code>
+        /// </example>
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="info"></param>
-        /// <returns></returns>
+        /// <param name="key">The key to search for in the notification's info.</param>
+        /// <param name="info">The output parameter that will hold the associated info if the key is found.</param>
+        /// <param name="logError">Optional parameter to log errors if the key is not found. Default is true.</param>
+        /// <returns>True if the key is found and info is retrieved; otherwise, false.</returns>
         public bool TryGetInfo(string key, out Object info, bool logError = true)
         {
-            // If 'Info' is null then there is no additional information.
+            // Key cannot be null.
+            if (key == null)
+            {
+                info = null;
+                if (logError)
+                {
+                    UnityEngine.Debug.LogError($"[Notification.TryGetInfo] Error: key is null for notification '{ID}'.");
+                }
+                return false;
+            }
+
+            string errorMessage;
             if (Info == null)
             {
                 info = null;
                 if (logError)
                 {
-                    UnityEngine.Debug.LogError($"Notification: '{ID}' does not contain info id: '{key}'.");
+                    errorMessage = $"[Notification.TryGetInfo] Error: key '{key}' not found for notification '{ID}'.\n" +
+                        $"Reason: info is null.";
+                    UnityEngine.Debug.LogError(errorMessage);
+                }
+                return false;
+            }
+            else if (Info.Count == 0)
+            {
+                info = null;
+                if (logError)
+                {
+                    errorMessage = $"[Notification.TryGetInfo] Error: key '{key}' not found for notification '{ID}'.\n" +
+                        $"Reason: info is empty.";
+                    UnityEngine.Debug.LogError(errorMessage);
+                }
+                return false;
+            }
+            else if (!Info.TryGetValue(key, out info))
+            {
+                info = null;
+                if (logError)
+                {
+                    errorMessage = $"[Notification.TryGetInfo] Error: key '{key}' not found for notification '{ID}'.\n" +
+                        $"Reason: info does not contain '{key}'.";
+                    UnityEngine.Debug.LogError(errorMessage);
                 }
                 return false;
             }
 
-            // Try to find the value corresponding to that 'key'.
-            return Info.TryGetValue(key, out info);
+            return true;
         }
 
         /// <summary>
-        /// Try to get the information stored with this <paramref name="key"/>.<br/>
-        /// Return true if the information exist and is of type <typeparamref name="T"/>, else false.
+        /// Tries to retrieve information of a specified type associated with a given key from the notification.<br/>
+        /// If the key is null or not found or if the type does not match, it logs an error (if logError is true) and returns false.<br/>
+        /// If the value is null and the type is a reference type, it logs a warning and returns true.<br/>
+        /// <br/>
+        /// <example>
+        /// Given a notification and a key, when the key is found in the notification's info and the type matches, then return true and the associated info.<br/>
+        /// <code>
+        /// Notification notification = new Notification("id", this, new() { { "key", "value" } });
+        /// bool result = notification.TryGetInfoT("key", out string info); // result = true, info = "value"
+        /// </code>
+        /// </example>
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="info"></param>
-        /// <param name="logError">Whether a log error will be display if no value is found.</param>
-        /// <returns></returns>
+        /// <typeparam name="T">The type of the information to retrieve.</typeparam>
+        /// <param name="key">The key to search for in the notification's info.</param>
+        /// <param name="info">The output parameter that will hold the associated info if the key is found and the type matches.</param>
+        /// <param name="logError">Optional parameter to log errors if the key is not found or the type does not match. Default is true.</param>
+        /// <returns>True if the key is found and info is retrieved; otherwise, false.</returns>
         public bool TryGetInfoT<T>(string key, out T info, bool logError = true)
         {
             if (!TryGetInfo(key, out object infoObject, logError))
@@ -96,20 +172,44 @@ namespace inetum.unityUtils.observation
                 return false;
             }
 
+            if (infoObject == null)
+            {
+                info = default;
+                string error;
+                Type type = typeof(T);
+                if (type.IsValueType || type.IsEnum)
+                {
+                    if (logError)
+                    {
+                        error =
+                        $"[Notification.TryGetInfoT] Error: notification '{ID}' does not contain key '{key}' of type {typeof(T)}.\n" +
+                        $"Type of the object is Unknown because the value is null.";
+                        UnityEngine.Debug.LogError(error);
+                    }
+                    return false;
+                } else
+                {
+                    if (logError)
+                    {
+                        error =
+                        $"[Notification.TryGetInfoT] Warning: notification '{ID}' has a null value for key '{key}'.\n" +
+                        $"The initial type of a null value is Unknown but you are trying to cast it in '{typeof(T)}'.";
+                        UnityEngine.Debug.LogWarning(error);
+                    }
+                    return true;
+                }
+            }
+
+
             // Try to cast the information.
             if (infoObject is not T infoT)
             {
                 info = default;
-                if (infoObject == null)
-                {
-                    // If infoObject is not T but is null then return true.
-                    return true;
-                }
-
                 if (logError)
                 {
-                    string error = $"Notification: '{ID}' does not contain info id: '{key}' of type {typeof(T)}.";
-                    error += $"\nType of the object is {infoObject.GetType()}\n";
+                    string error = 
+                    $"[Notification.TryGetInfoT] Error: notification '{ID}' does not contain key '{key}' of type {typeof(T)}.\n" +
+                    $"Type of the object is {infoObject.GetType()}.";
                     UnityEngine.Debug.LogError(error);
                 }
                 return false;

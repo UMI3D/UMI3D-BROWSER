3c0f965020766114c0b3c04562f4a326528c31ad
Guyon Nolan <133751460+nguyon-inetum@users.noreply.github.com>
2025-01-02 15:41:12 +0100

    chore(Toggle): Add comment to factory

--- UMI3D-BROWSER/Assets/Runtime/Toggle/Public/01-Scripts/ToggleFactory.cs
+++ UMI3D-BROWSER/Assets/Runtime/Toggle/Public/01-Scripts/ToggleFactory.cs
@@ -19,6 +19,9 @@ using UnityEngine;
 
 namespace umi3d.browserRuntime.ui.toggle
 {
+    /// <summary>
+     /// Factory managing a pool of toggle.
+     /// </summary>
     public class ToggleFactory : MonoBehaviour
     {
         [SerializeField] ToggleModelContainer _togglePrefab;
@@ -27,6 +30,23 @@ namespace umi3d.browserRuntime.ui.toggle
 
         public int AvailableToggleCount => _lstTogglesAvaible.Count;
 
+        /// <summary>
+        /// Gets an available toggle from the pool or creates a new one if none are available, sets its label and value, and activates it.<br/>
+        /// <br/>
+        /// <example>
+        /// Given a parent transform, label, and value, when getting or creating a toggle, then the toggle is set up with the specified parameters and activated.
+        /// <code>
+        /// var toggle = _toggleFactory.GetOrCreateToggle(parent, "TestLabel", true);
+        /// // toggle is active
+        /// // toggle's label is "TestLabel"
+        /// // toggle's value is true
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="parent">The parent transform to which the toggle will be attached.</param>
+        /// <param name="label">The label to set for the toggle. Default is an empty string.</param>
+        /// <param name="value">The value to set for the toggle. Default is false.</param>
+        /// <returns>The created or retrieved toggle GameObject.</returns>
         public GameObject GetOrCreateToggle(Transform parent, string label = "", bool value = false)
         {
             if (!_lstTogglesAvaible.TryDequeue(out var toggleModelContainer))
@@ -41,6 +61,19 @@ namespace umi3d.browserRuntime.ui.toggle
             return toggleModelContainer.gameObject;
         }
 
+        /// <summary>
+        /// Returns the specified toggle GameObject to the pool, deactivates it, and reassigns its parent.<br/>
+        /// <br/>
+        /// <example>
+        /// Given a toggle GameObject, when returning it to the pool, then the toggle is deactivated and added back to the pool.
+        /// <code>
+        /// _toggleFactory.Return(toggleGameObject);
+        /// // toggleGameObject is inactive
+        /// // toggleGameObject is added back to the pool
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="toggleGameObject">The toggle GameObject to return to the pool.</param>
         public void Return(GameObject toggleGameObject)
         {
             var toggleModelContainer = toggleGameObject.GetComponent<ToggleModelContainer>();

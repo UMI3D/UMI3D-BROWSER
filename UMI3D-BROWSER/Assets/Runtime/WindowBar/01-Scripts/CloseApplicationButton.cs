9a94122c02fd785dd8251d5bcf4ead092c405420
Romain T. <romain.tirbisch@inetum.com>
2025-01-17 11:41:10 +0100

    inetum.unityUtils.lifeCycle
    - Now the process of quitting can be trigger by Application.Quit.

--- UMI3D-BROWSER/Assets/Runtime/WindowBar/01-Scripts/CloseApplicationButton.cs
+++ UMI3D-BROWSER/Assets/Runtime/WindowBar/01-Scripts/CloseApplicationButton.cs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using inetum.unityUtils.lifeCycle;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -35,7 +36,7 @@ namespace umi3d.browserRuntime.ui.windowBar
 
         private void Close()
         {
-            Application.Quit();
+            Quitting.instance.Quit(this);
             EventSystem.current.SetSelectedGameObject(null);
         }
     }
